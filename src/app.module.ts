735e089590c8db6d68527d79a47fb2c17efba454
Golam Mostafa <gmnayeemgb@gmail.com>
2024-12-09 15:55:21 +0600

    add crud app

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.local.env',
+        }),
+      ],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<boolean>('DB_SYNC'),
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor() {
+    console.log('Hi, I am app module.');
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,46 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService { 
+  // inject user repository
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+
+  create(createUserDto: CreateUserDto) : Promise<User> {
+    const user:User = new User();
+
+    user.fullName = createUserDto.fullName;
+    user.email = createUserDto.email;
+    user.age = createUserDto.age;
+
+    return this.userRepository.save(user);
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneById(id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const user:User = new User();
+
+    user.fullName = updateUserDto.fullName;
+    user.email = updateUserDto.email;
+    user.age = updateUserDto.age;
+    user.id = id;
+
+    return this.userRepository.save(user);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
